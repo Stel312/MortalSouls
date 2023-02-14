d9415e60dcb9e5bf69a5123a05c2feb94bd3d0a3
Stel312 <nstelken312@gmail.com>
2023-02-14 17:24:07 -0600

    air slash

--- src/main/java/online/mortalsouls/mortalsouls/integration/epicfight/MortalAnimations.java
+++ src/main/java/online/mortalsouls/mortalsouls/integration/epicfight/MortalAnimations.java
@@ -15,6 +15,7 @@ import yesman.epicfight.gameasset.Armatures;
 public class MortalAnimations {
     public static StaticAnimation GREATSHIELD_DUAL_BLOCK;
     public static StaticAnimation GREATSHIELD_COMBO1;
+    public static StaticAnimation GREATSHIELD_AIR_SLASH;
     public static StaticAnimation GREATHAMMER_IDLE;
     public static StaticAnimation GREATHAMMER_GUARD;
 
@@ -31,7 +32,10 @@ public class MortalAnimations {
                 .addProperty(AnimationProperty.StaticAnimationProperty.PLAY_SPEED, .5F);
         GREATSHIELD_COMBO1 = new BasicAttackAnimation(0.13F, 0.0F, 0.3F, 0.4F, null,
                 Armatures.BIPED.toolR, "biped/combat/greatshield_combo1", Armatures.BIPED)
-                .addProperty(AnimationProperty.StaticAnimationProperty.PLAY_SPEED, .8F);
+                .addProperty(AnimationProperty.StaticAnimationProperty.PLAY_SPEED, .9F);
+        GREATSHIELD_AIR_SLASH = new BasicAttackAnimation(0.13F, 0.0F, 0.3F, 0.4F, null,
+                Armatures.BIPED.toolR, "biped/combat/greatshield_air_slash", Armatures.BIPED)
+                .addProperty(AnimationProperty.StaticAnimationProperty.PLAY_SPEED, 1.5F);
         GREATHAMMER_IDLE = new StaticAnimation(true, "biped/living/greathammer_idle", Armatures.BIPED);
         GREATHAMMER_GUARD = new StaticAnimation(true, "biped/skill/guard_greathammer", Armatures.BIPED);
 
