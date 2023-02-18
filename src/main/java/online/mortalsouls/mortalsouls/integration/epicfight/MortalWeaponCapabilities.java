5ecd77e21639e1612596cf7027baa5230fe29f67
Stel312 <nstelken312@gmail.com>
2023-02-18 16:16:42 -0600

    add getter

--- src/main/java/online/mortalsouls/mortalsouls/integration/epicfight/MortalWeaponCapabilities.java
+++ src/main/java/online/mortalsouls/mortalsouls/integration/epicfight/MortalWeaponCapabilities.java
@@ -5,21 +5,24 @@ import yesman.epicfight.gameasset.Animations;
 import yesman.epicfight.skill.GuardSkill;
 import yesman.epicfight.world.capabilities.entitypatch.player.PlayerPatch;
 import yesman.epicfight.world.capabilities.item.CapabilityItem;
-import yesman.epicfight.world.capabilities.item.Style;
 import yesman.epicfight.world.capabilities.item.WeaponCapability;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class MortalWeaponCapabilities extends WeaponCapability {
-    private final Map<String, GuardObject> map = new HashMap<>();
+    private final Map<String, GuardObject> guardMap = new HashMap<>();
     protected MortalWeaponCapabilities(CapabilityItem.Builder builder) {
         super(builder);
-        map.put(this.getWeaponCategory().toString()+ Styles.TWO_HAND, new GuardObject(Animations.GREATSWORD_GUARD_HIT, Animations.GREATSWORD_GUARD_BREAK, Animations.GREATSWORD_GUARD_HIT));
+        guardMap.put(this.getWeaponCategory().toString()+ Styles.TWO_HAND, new GuardObject(Animations.GREATSWORD_GUARD_HIT, Animations.GREATSWORD_GUARD_BREAK, Animations.GREATSWORD_GUARD_HIT));
     }
 
     @Override
     public StaticAnimation getGuardMotion(GuardSkill skill, GuardSkill.BlockType blockType, PlayerPatch<?> playerpatch) {
-        return map.get(this.getWeaponCategory().toString()+this.getStyle(playerpatch).toString()).getGuardAnimation(blockType);
+        return guardMap.get(this.getWeaponCategory().toString()+this.getStyle(playerpatch)).getGuardAnimation(blockType);
+    }
+
+    public Map<String, GuardObject> getGuardMap() {
+        return guardMap;
     }
 }
