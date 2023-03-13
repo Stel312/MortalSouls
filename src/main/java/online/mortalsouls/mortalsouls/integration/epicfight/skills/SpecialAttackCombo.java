b82d38be3eff7db0f43efa68bd26b0cf4f30b9a8
Stel312 <nstelken312@gmail.com>
2023-03-12 22:51:01 -0500

    Fix

--- src/main/java/online/mortalsouls/mortalsouls/integration/epicfight/skills/SpecialAttackCombo.java
+++ src/main/java/online/mortalsouls/mortalsouls/integration/epicfight/skills/SpecialAttackCombo.java
@@ -8,8 +8,8 @@ import yesman.epicfight.world.capabilities.entitypatch.player.ServerPlayerPatch;
 
 public class SpecialAttackCombo extends WeaponInnateSkill {
     private final StaticAnimation[] attackAnimations;
-    private final SkillDataManager.SkillDataKey<Integer> COMBO_COUNTER = SkillDataManager.SkillDataKey
-            .createDataKey(SkillDataManager.ValueType.INTEGER);
+    private final SkillDataManager.SkillDataKey<Integer> combo = (SkillDataManager.SkillDataKey<Integer>) SkillDataManager.SkillDataKey
+            .findById(0);
 
     public SpecialAttackCombo(Builder<? extends Skill> builder, StaticAnimation... animations) {
         super(builder);
@@ -30,12 +30,11 @@ public class SpecialAttackCombo extends WeaponInnateSkill {
     public void executeOnServer(ServerPlayerPatch executer, FriendlyByteBuf args) {
         super.executeOnServer(executer, args);
         SkillDataManager dataManager = executer.getSkill(SkillCategories.BASIC_ATTACK).getDataManager();
-        int combo = (int) dataManager.getDataValue(COMBO_COUNTER);
-
+        int combo = dataManager.getDataValue(this.combo);
 
         if (attackAnimations != null && attackAnimations[combo] != null) {
             executer.playAnimationSynchronized(attackAnimations[combo], 0);
-            dataManager.setData(COMBO_COUNTER, 0);
+            dataManager.setData(this.combo, 0);
         }
     }
 
