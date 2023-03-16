9e21ad4dba7dae309643123ad6952293db27ed52
Stel312 <nstelken312@gmail.com>
2023-03-16 13:53:22 -0500

    cleanup

--- src/main/java/online/mortalsouls/mortalsouls/integration/epicfight/skills/SpecialAttackCombo.java
+++ src/main/java/online/mortalsouls/mortalsouls/integration/epicfight/skills/SpecialAttackCombo.java
@@ -3,9 +3,11 @@ package online.mortalsouls.mortalsouls.integration.epicfight.skills;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
-import org.apache.logging.log4j.core.jmx.Server;
 import yesman.epicfight.api.animation.types.StaticAnimation;
-import yesman.epicfight.skill.*;
+import yesman.epicfight.skill.Skill;
+import yesman.epicfight.skill.SkillCategories;
+import yesman.epicfight.skill.SkillDataManager;
+import yesman.epicfight.skill.WeaponInnateSkill;
 import yesman.epicfight.world.capabilities.entitypatch.player.PlayerPatch;
 import yesman.epicfight.world.capabilities.entitypatch.player.ServerPlayerPatch;
 
@@ -15,6 +17,9 @@ public class SpecialAttackCombo extends WeaponInnateSkill {
     private final StaticAnimation dashAttack;
     private final SkillDataManager.SkillDataKey<Integer> combo = (SkillDataManager.SkillDataKey<Integer>) SkillDataManager.SkillDataKey
             .findById(0);
+    private final Skill specialSkill = MortalSkills.lightningSkill;
+
+
 
     public SpecialAttackCombo(Builder<? extends Skill> builder,StaticAnimation dashAttack,
                               StaticAnimation jumpAttack, StaticAnimation... heavyAnimations) {
@@ -35,33 +40,25 @@ public class SpecialAttackCombo extends WeaponInnateSkill {
         ServerPlayer serverPlayer = executer.getOriginal();
         SkillDataManager dataManager = executer.getSkill(SkillCategories.BASIC_ATTACK).getDataManager();
         int combo = dataManager.getDataValue(this.combo);
-        if(serverPlayer.isShiftKeyDown())
-        {
-            MortalSkills.lightningSkill.executeOnServer(executer,args);
-        } else if (!serverPlayer.isOnGround() && serverPlayer.fallDistance <1) {
+        if(serverPlayer.isShiftKeyDown()) {
+            specialSkill.executeOnServer(executer,args);
+        } else if (!serverPlayer.isOnGround() && serverPlayer.fallDistance <2) {
             executer.playAnimationSynchronized(jumpAttack,0);
-            
         } else if(serverPlayer.isSprinting()) {
             executer.playAnimationSynchronized(dashAttack, 0);
-        }
-        else if (attackAnimations != null && attackAnimations[combo] != null) {
+        } else if (attackAnimations != null && attackAnimations[combo] != null) {
             executer.playAnimationSynchronized(attackAnimations[combo], 0);
             dataManager.setData(this.combo, 0);
         }
     }
 
     @Override
-    public void cancelOnServer(ServerPlayerPatch executer, FriendlyByteBuf args) {
-        super.cancelOnServer(executer, args);
-    }
-
-    @Override
     public ResourceLocation getSkillTexture() {
-        return MortalSkills.lightningSkill.getSkillTexture();
+        return specialSkill.getSkillTexture();
     }
 
     @Override
     public WeaponInnateSkill registerPropertiesToAnimation() {
-        return null;
+        return this;
     }
 }
